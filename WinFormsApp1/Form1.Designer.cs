5d2dbcdc8baaae1a6dbb273a9b986f7f0438474e
farhad aslanilani <teacherfarhad.net@hotmail.com>
2024-12-26 20:44:05 +0330

    Add New Win Form

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private Button buttun1;
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            buttun1 = new Button();
+            SuspendLayout();
+            // 
+            // buttun1
+            // 
+            buttun1.Location = new Point(0, 0);
+            buttun1.Name = "buttun1";
+            buttun1.Size = new Size(100, 100);
+            buttun1.TabIndex = 0;
+            buttun1.Text = "Click Me";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1051, 543);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System.Timers;
+using OfficeOpenXml;
+using System.IO;
+using System.Diagnostics;
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Stopwatch stopwatch;
+        private List<(string Time, string Data)> dataList;
+        private ExcelPackage excelPackage;
+        private ExcelWorksheet worksheet;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stopwatch = new Stopwatch();
+            dataList = new List<(string Time, string Data)>();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // تنظیم مجوز
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Excel Files|*.xlsx";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+                    excelPackage = new ExcelPackage(fileInfo);
+                    worksheet = excelPackage.Workbook.Worksheets[0];
+                    Task.Run(() => CollectData());
+                }
+            }
+        }
+        private async Task CollectData()
+        {
+            try
+            {
+                stopwatch.Start();
+                bool ok = true;
+                while (dataList.Count <= 200)
+                {
+                    if (stopwatch.ElapsedMilliseconds == 20)
+                    {
+                        string currentTime = DateTime.Now.ToString("ss.fff");
+                        string data = "Data ";
+                        dataList.Add((currentTime, data));
+                        if (dataList.Count % 100 == 0)
+                        {
+                            await Task.Delay(1);
+                        }
+                        stopwatch.Restart();
+
+                    }
+
+                }
+                WriteDataToExcel();
+            }
+            catch (Exception x)
+            {
+
+                throw;
+            }
+        }
+
+        private void WriteDataToExcel()
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                for (int i = 0; i < dataList.Count; i++)
+                {
+                    worksheet.Cells[i + 1, 1].Value = dataList[i].Time;
+                    worksheet.Cells[i + 1, 2].Value = dataList[i].Data;
+                }
+                excelPackage.Save();
+                excelPackage.Dispose();
+            });
+        }
+    }
+}
